ebb0125d192f4a47636eecdd696a7277b0f872aa
dokiguru <technology@meltingsoft.com>
2019-05-07 13:46:14 +0200

    - Creating hosted application service.

---
+++ src/Framework.Runtime/Application/Services/THostedAppService.cs
@@ -0,0 +1,43 @@
+﻿using BindOpen.Framework.Core.Application.Scopes;
+using BindOpen.Framework.Runtime.Application.Configuration;
+using BindOpen.Framework.Runtime.Application.Hosts;
+using BindOpen.Framework.Runtime.Application.Options;
+
+namespace BindOpen.Framework.Runtime.Application.Services
+{
+    /// <summary>
+    /// This class represents an application host.
+    /// </summary>
+    public class THostedAppService<Q> : TAppService<Q>, ITAppHosted<Q>
+        where Q : IAppConfiguration, new()
+    {
+        /// <summary>
+        /// The host of this instance.
+        /// </summary>
+        public ITAppHost<Q> Host { get; set; }
+
+        // ------------------------------------------
+        // CONSTRUCTORS
+        // ------------------------------------------
+
+        #region Constructors
+
+        /// <summary>
+        /// Instantiates a new instance of the THostedAppService class.
+        /// </summary>
+        public THostedAppService() : this(null, null)
+        {
+        }
+
+        /// <summary>
+        /// Instantiates a new instance of the THostedAppService class.
+        /// </summary>
+        public THostedAppService(
+            IAppHostScope appScope,
+            ITAppHostOptions<Q> options) : base(appScope, options)
+        {
+        }
+
+        #endregion
+    }
+}
