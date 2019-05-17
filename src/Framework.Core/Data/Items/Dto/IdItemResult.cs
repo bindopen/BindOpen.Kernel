21b24b69cd0aef663a8d9a119e676bf6634ff50c
dokiguru <technology@meltingsoft.com>
2019-05-17 13:14:42 +0200

    - Fixing bug in log event conversion to Api log Dto.

---
+++ src/Framework.Core/Data/Items/Dto/IdItemResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml.Serialization;
+using BindOpen.Framework.Core.Data.Common;
+
+namespace BindOpen.Framework.Core.Data.Items.Dto
+{
+    /// <summary>
+    /// This class represents the ID item result.
+    /// </summary>
+    [Serializable()]
+    [XmlType("IdItemResult", Namespace = "https://bindopen.org/xsd")]
+    [XmlRoot("idItemResult", Namespace = "https://bindopen.org/xsd", IsNullable = false)]
+    public class IdItemResult
+    {
+        [XmlElement("id")]
+        public string Id { get; set; }
+
+        [XmlElement("status")]
+        public ResourceStatus Status { get; set; }
+
+        public IdItemResult(string id, ResourceStatus status)
+        {
+            Id = id;
+            Status = status;
+        }
+    }
+}
---
+++ src/Framework.Core/Data/Items/Dto/NameItemResult.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Xml.Serialization;
+using BindOpen.Framework.Core.Data.Common;
+
+namespace BindOpen.Framework.Core.Data.Items.Dto
+{
+    /// <summary>
+    /// This class represents the named item result.
+    /// </summary>
+    [Serializable()]
+    [XmlType("NameItemResult", Namespace = "https://bindopen.org/xsd")]
+    [XmlRoot("nameItemResult", Namespace = "https://bindopen.org/xsd", IsNullable = false)]
+    public class NameItemResult
+    {
+        [XmlElement("name")]
+        public string Name { get; set; }
+
+        [XmlElement("status")]
+        public ResourceStatus Status { get; set; }
+
+        public NameItemResult(string name, ResourceStatus status)
+        {
+            Name = name;
+            Status = status;
+        }
+    }
+}
