1eb82c900c75e98310e62964b571fac80d232fc9
xgonier <xgonier@meltingsoft.com>
2020-03-29 01:14:45 +0100

    - Fixing bug in CreateScalar method with item is null.

--- test/BindOpen.Tests.Runtime/Application/Hosts/AppHostTest.cs
+++ test/BindOpen.Tests.Runtime/Application/Hosts/AppHostTest.cs
@@ -1,4 +1,6 @@
-﻿using NUnit.Framework;
+﻿using BindOpen.Application.Scopes;
+using BindOpen.Data.Stores;
+using NUnit.Framework;
 
 namespace BindOpen.Tests.Core.Application.Hosts
 {
@@ -11,9 +13,29 @@ namespace BindOpen.Tests.Core.Application.Hosts
         }
 
         [Test]
-        public void TestLoggers()
+        public void TestAppHost()
         {
             Assert.That(GlobalVariables.AppHost.IsLoaded, "Application host not load failed");
+        }
+
+        [Test]
+        public void TestAppHostWithNoOptions()
+        {
+            var appHost = BdoHostFactory.CreateBindOpenDefaultHost(
+                options => options
+                    .SetRootFolder(@"..\..")
+                    .AddDataStore(store => store
+                        .RegisterDatasources(m => m.AddFromConfiguration(options)))
+                    .AddDefaultConsoleLogger()
+                    .AddDefaultFileLogger());
+
+            Assert.That(appHost.IsLoaded, "Application host not load failed");
+
+            Assert.That(appHost.Options.HostSettings.ApplicationInstanceName == "test.console", "Application host settings not loaded correctly");
+
+            Assert.That(appHost.Options.HostSettings.LogsExpirationDayNumber == 1, "Application host settings not loaded correctly");
+
+            Assert.That(appHost.Options.AppSettings.Get<string>("test.folderPath") != null, "Application host settings not loaded correctly");
         }
     }
 }
