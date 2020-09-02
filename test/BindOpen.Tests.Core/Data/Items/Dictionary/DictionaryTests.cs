971af20235af0265020251713891397362072849
xgonier <xgonier@meltingsoft.com>
2020-09-02 08:39:20 +0200

    - Fixing bug with default dictionary value

--- test/BindOpen.Tests.Core/Data/Items/Dictionary/DictionaryTests.cs
+++ test/BindOpen.Tests.Core/Data/Items/Dictionary/DictionaryTests.cs
@@ -1,12 +1,18 @@
-﻿using BindOpen.Data.Items;
+﻿using BindOpen.Data.Helpers.Serialization;
+using BindOpen.Data.Items;
+using BindOpen.System.Diagnostics;
 using NUnit.Framework;
+using System.IO;
 
 namespace BindOpen.Tests.Core.Data.Items
 {
     [TestFixture, Order(210)]
     public class DictionaryTests
     {
+        private readonly string _filePath = GlobalVariables.WorkingFolder + "Dicionary.xml";
         dynamic valueSet;
+        private IDictionaryDataItem _dictionary = null;
+
 
         [OneTimeSetUp]
         public void OneTimeSetUp()
@@ -14,14 +20,19 @@ namespace BindOpen.Tests.Core.Data.Items
             valueSet = new { value1 = "toto", value2 = "totomax" };
         }
 
-        [Test, Order(1)]
-        public void CreateDictionaryTest()
+        private void Test(IDictionaryDataItem dictionary)
         {
-            var dictionary = ItemFactory.CreateDictionary(("value1", valueSet.value1), ("value2", valueSet.value2));
-
             Assert.That(
                 dictionary["value1"] == valueSet.value1
                 && dictionary["value2"] == valueSet.value2, "Bad dictionary creation");
+        }
+
+        [Test, Order(1)]
+        public void CreateDictionaryTest()
+        {
+            _dictionary = ItemFactory.CreateDictionary(("value1", valueSet.value1), ("value2", valueSet.value2));
+
+            Test(_dictionary);
         }
 
         [Test, Order(2)]
@@ -34,6 +45,47 @@ namespace BindOpen.Tests.Core.Data.Items
 
             Assert.That(
                 dictionary1.Equals(dictionary2), "Bad dictionary equal funtion");
+        }
+
+        [Test, Order(3)]
+        public void SaveDictionaryTest()
+        {
+            if (_dictionary == null)
+            {
+                CreateDictionaryTest();
+            }
+
+            var log = new BdoLog();
+            _dictionary.SaveXml(_filePath, log);
+
+            string xml = string.Empty;
+            if (log.HasErrorsOrExceptions())
+            {
+                xml = ". Result was '" + log.ToXml() + "'";
+            }
+            Assert.That(!log.HasErrorsOrExceptions(), "Element set saving failed" + xml);
+        }
+
+        [Test, Order(4)]
+        public void LoadDictionaryTest()
+        {
+            var log = new BdoLog();
+
+            if (_dictionary == null || !File.Exists(_filePath))
+            {
+                SaveDictionaryTest();
+            }
+
+            var elementSet = XmlHelper.Load<DictionaryDataItem>(_filePath, log: log);
+
+            string xml = string.Empty;
+            if (log.HasErrorsOrExceptions())
+            {
+                xml = ". Result was '" + log.ToXml() + "'";
+            }
+            Assert.That(!log.HasErrorsOrExceptions(), "Element set loading failed" + xml);
+
+            Test(elementSet);
         }
     }
 }
