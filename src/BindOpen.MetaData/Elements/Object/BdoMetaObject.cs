97be6f355f9c571310b8b7124b48c650274bff8c
xgonier <xgonier@meltingsoft.com>
2023-01-12 16:15:38 +0100

    - Renaming project BindOpen.MetaData

---
+++ src/BindOpen.MetaData/Elements/Object/BdoMetaObject.cs
@@ -0,0 +1,144 @@
+﻿using BindOpen.Meta.Items;
+using System.Reflection;
+
+namespace BindOpen.Meta.Elements
+{
+    /// <summary>
+    /// This class represents a object element that is an element whose items are entities.
+    /// </summary>
+    public class BdoMetaObject :
+        TBdoMetaElement<IBdoMetaObject, IBdoMetaObjectSpec, object>,
+        IBdoMetaObject
+    {
+        // --------------------------------------------------
+        // CONSTRUCTORS
+        // --------------------------------------------------
+
+        #region Constructors
+
+        /// <summary>
+        /// Initializes a new instance of the ObjectElement class.
+        /// </summary>
+        public BdoMetaObject() : this(null)
+        {
+        }
+
+        /// <summary>
+        /// Initializes a new instance of the ObjectElement class.
+        /// </summary>
+        /// <param name="name">The name to consider.</param>
+        /// <param name="id">The ID to consider.</param>
+        public BdoMetaObject(string name = null, string id = null)
+            : base(name, "objectElem_", id)
+        {
+        }
+
+        #endregion
+
+        // --------------------------------------------------
+        // IObjectElement Implementation
+        // --------------------------------------------------
+
+        #region Properties
+
+        /// <summary>
+        /// The definition unique ID of this instance.
+        /// </summary>
+        public string DefinitionUniqueId { get; set; }
+
+        public IBdoMetaObject WithDefinitionUniqueId(string definitionUniqueId)
+        {
+            DefinitionUniqueId = definitionUniqueId;
+
+            return this;
+        }
+
+        /// <summary>
+        /// The class full name of this instance.
+        /// </summary>
+        public string ClassFullName { get; set; }
+
+        public IBdoMetaObject WithClassFullName(string classFullName)
+        {
+            ClassFullName = classFullName;
+
+            return this;
+        }
+
+        #endregion
+
+        // --------------------------------------------------
+        // ITBdoElement implementation
+        // --------------------------------------------------
+
+        #region Items
+
+        // Specification ---------------------
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        IBdoMetaElementSpec IBdoMetaElement.NewSpecification()
+        {
+            return NewSpecification();
+        }
+
+        // Items ----------------------------
+
+        /// <summary>
+        /// Adds a new single item of this instance.
+        /// </summary>
+        /// <param name="objs">The string item of this instance.</param>
+        /// <remarks>Items of this instance must be allowed and must not be forbidden. Otherwise, the items will be the default ones..</remarks>
+        /// <returns>Returns True if the specified has been well added.</returns>
+        public override IBdoMetaObject WithItem(params object[] objs)
+        {
+            if (objs != null)
+            {
+                base.WithItem(objs);
+                if (_item is BdoItem)
+                {
+                    var type = _item.GetType();
+                    Assembly assembly = type.Assembly;
+                    ClassFullName = type.FullName.ToString()
+                        + (assembly == null ? string.Empty : "," + assembly.GetName().Name);
+                }
+            }
+
+            return this;
+        }
+
+        /// <summary>
+        /// Returns a text node representing this instance.
+        /// </summary>
+        /// <returns></returns>
+        public override string ToString()
+        {
+            return "";
+        }
+
+        #endregion
+
+        // --------------------------------------------------
+        // CLONING
+        // --------------------------------------------------
+
+        #region Cloning
+
+        /// <summary>
+        /// Clones this instance.
+        /// </summary>
+        /// <returns>Returns a cloned instance.</returns>
+        public override object Clone(params string[] areas)
+        {
+            BdoMetaObject aObjectElement = base.Clone(areas) as BdoMetaObject;
+            //if (DataSchemreference != null)
+            //    aObjectElement.DataSchemreference = DataSchemreference.Clone() as DataHandler;
+
+            return aObjectElement;
+        }
+
+        #endregion
+    }
+}
---
+++ src/BindOpen.MetaData/Factories/BdoMeta_Elements.cs
@@ -0,0 +1,97 @@
+﻿using BindOpen.Extensions.Connecting;
+using BindOpen.Extensions.Modeling;
+using BindOpen.Meta.Elements;
+
+namespace BindOpen.Meta
+{
+    /// <summary>
+    /// This static class provides methods to create data elements.
+    /// </summary>
+    public static partial class BdoMeta
+    {
+        // Static creators -------------------------
+
+        /// <summary>
+        /// Creates a data element with specified items.
+        /// </summary>
+        /// <param name="name">The name to consider.</param>
+        /// <param name="items">The items to consider.</param>
+        public static BdoMetaElement NewElement(
+            string name,
+            params object[] items)
+        {
+            return NewElement(name, DataValueTypes.Any, items);
+        }
+
+        /// <summary>
+        /// Creates a data element with specified items.
+        /// </summary>
+        /// <param name="name">The name to consider.</param>
+        /// <param name="items">The items to consider.</param>
+        public static BdoMetaElement NewElement(
+            string name,
+            DataValueTypes valueType,
+            params object[] items)
+        {
+            BdoMetaElement element = default;
+
+            if (valueType == DataValueTypes.Any || valueType == DataValueTypes.None)
+            {
+                valueType = items.GetValueType();
+            }
+
+            if (valueType.IsScalar())
+            {
+                element = NewScalar(name, valueType, items);
+            }
+            else
+            {
+                string definitionUniqueId;
+                switch (valueType)
+                {
+                    case DataValueTypes.Carrier:
+                        definitionUniqueId = ((items.Length > 0 ? items[0] : null) as IBdoCarrierConfiguration)?.DefinitionUniqueId;
+                        element = NewCarrier(name, definitionUniqueId);
+                        break;
+                    case DataValueTypes.Datasource:
+                        definitionUniqueId = ((items.Length > 0 ? items[0] : null) as IBdoConnectorConfiguration)?.DefinitionUniqueId;
+                        element = NewSource(name, definitionUniqueId);
+                        break;
+                    case DataValueTypes.Object:
+                        element = NewObject(name, items);
+                        break;
+                }
+
+                if (items != null)
+                {
+                    element?.WithItem(items);
+                }
+            }
+
+            return element;
+        }
+
+        /// <summary>
+        /// Creates a data element with specified items.
+        /// </summary>
+        /// <param name="name">The name to consider.</param>
+        /// <param name="items">The items to consider.</param>
+        public static BdoMetaElement NewElement<T>(
+            string name,
+            params object[] items)
+        {
+            var type = typeof(T);
+
+            var valueType = type.GetValueType();
+            var el = NewElement(name, valueType, items);
+
+            if (el.GetType().IsAssignableFrom(typeof(IBdoMetaObjectSpec)))
+            {
+                var objectEl = el as IBdoMetaObjectSpec;
+                objectEl.ClassFilter.AddedValues.Add(el.GetType().ToString());
+            }
+
+            return el;
+        }
+    }
+}
