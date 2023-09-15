d2680a31a65578363c76783839a32f73150b32a1
xgonier <xgonier@meltingsoft.com>
2023-09-15 21:11:27 +0200

    - Fix bug in using.

--- src/Scoping.Extensions/Tasks/BdoTask.cs
+++ src/Scoping.Extensions/Tasks/BdoTask.cs
@@ -1,5 +1,4 @@
-﻿using BindOpen.Kernel.Abstractions._Core.Enums;
-using BindOpen.Kernel.Data.Meta;
+﻿using BindOpen.Kernel.Data.Meta;
 using BindOpen.Kernel.Logging;
 using System.Threading;
 using System.Threading.Tasks;
