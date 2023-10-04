24e164daca575186d678a12aec7dc8953e935435
xgonier <xgonier@meltingsoft.com>
2023-10-04 11:34:22 +0200

    - Test build of @bindopen-kernel-dtos

--- src/Data/_Core/Factories/BdoData_Objects_Sets.cs
+++ src/Data/_Core/Factories/BdoData_Objects_Sets.cs
@@ -1,4 +1,7 @@
-﻿namespace BindOpen.Kernel.Data
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace BindOpen.Kernel.Data
 {
     /// <summary>
     /// This class represents a data item factory.
@@ -30,6 +33,34 @@
             where T : IReferenced
         {
             return NewItemSet<TBdoSet<T>, T>(items);
+        }
+
+        /// <summary>
+        /// Instantiates a new instance of the DataList class.
+        /// </summary>
+        /// <param key="items">The items to consider.</param>
+        /// <typeparam name="Q">The data item set type to consider.</typeparam>
+        /// <typeparam name="T">The identified data item to consider.</typeparam>
+        public static Q ToBdoItemSet<Q, T>(
+            this IEnumerable<T> enumerable)
+            where Q : class, ITBdoSet<T>, new()
+            where T : IReferenced
+        {
+            var set = NewItemSet<Q, T>(enumerable?.ToArray());
+            return set;
+        }
+
+        /// <summary>
+        /// Instantiates a new instance of the DataList class.
+        /// </summary>
+        /// <param key="items">The items to consider.</param>
+        /// <typeparam name="T">The class of the named data items.</typeparam>
+        public static TBdoSet<T> ToBdoItemSet<T>(
+            this IEnumerable<T> enumerable)
+            where T : IReferenced
+        {
+            var set = NewItemSet(enumerable?.ToArray());
+            return set;
         }
     }
 }
