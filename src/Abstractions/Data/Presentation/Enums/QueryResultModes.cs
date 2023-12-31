6f767ff4249aa41d77f93a30f9077c0ff8e44eaf
xgonier <xgonier@meltingsoft.com>
2023-12-31 08:44:37 +0100

    - Data presentation lyaer was added

---
+++ src/Abstractions/Data/Presentation/Enums/QueryResultModes.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Xml.Serialization;
+
+namespace BindOpen.Data
+{
+    /// <summary>
+    /// This enumeration lists the possible modes of query result.
+    /// </summary>
+    [Flags]
+    [XmlType("QueryResultModes", Namespace = "https://storage.bindopen.org/xsd/bindopen/kernel")]
+    public enum QueryResultModes
+    {
+        /// <summary>
+        /// Full.
+        /// </summary>
+        Full = 0,
+
+        /// <summary>
+        /// Partial.
+        /// </summary>
+        Partial = 0x01 << 0,
+
+        /// <summary>
+        /// Quick.
+        /// </summary>
+        Quick = 0x01 << 1,
+
+        /// <summary>
+        /// Any.
+        /// </summary>
+        Any = Quick | Partial | Full
+    }
+}
