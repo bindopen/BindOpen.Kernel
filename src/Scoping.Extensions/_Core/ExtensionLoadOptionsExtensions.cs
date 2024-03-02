65c03546da42814f38f8616eddb9173792f8f1ec
xgonier <xgonier@meltingsoft.com>
2024-03-02 18:48:43 +0100

    - Put AddAssemblies into Scoping.Extensions package.

---
+++ src/Scoping.Extensions/_Core/ExtensionLoadOptionsExtensions.cs
@@ -0,0 +1,50 @@
+﻿using BindOpen.Data;
+using BindOpen.Data.Assemblies;
+using System.Collections.Generic;
+
+namespace BindOpen.Scoping
+{
+    /// <summary>
+    /// This class represents a data element set.
+    /// </summary>
+    public static partial class ExtensionLoadOptionsExtensions
+    {
+        public static T AddAssemblies<T>(
+            this T obj,
+            params IBdoAssemblyReference[] references)
+            where T : IExtensionLoadOptions
+        {
+            if (obj != null)
+            {
+                obj.References ??= new List<IBdoAssemblyReference>();
+                foreach (var reference in references)
+                {
+                    obj.References.Add(reference);
+                }
+            }
+            return obj;
+        }
+
+        public static IExtensionLoadOptions AddAssemblyFrom<Q>(
+            this IExtensionLoadOptions options)
+        {
+            if (options != null)
+            {
+                options.References ??= new List<IBdoAssemblyReference>();
+                options.References.Add(BdoData.AssemblyFrom<Q>());
+            }
+            return options;
+        }
+
+        public static T AddAllAssemblies<T>(
+            this T obj)
+            where T : IExtensionLoadOptions
+        {
+            if (obj != null)
+            {
+                obj.References = new List<IBdoAssemblyReference>() { BdoData.AssemblyAsAll() };
+            }
+            return obj;
+        }
+    }
+}
