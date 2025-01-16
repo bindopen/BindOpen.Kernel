af030afe38de7e216fe999a2c5dee077a08dc8fd
xgonier <xgonier@meltingsoft.com>
2025-01-16 08:46:57 +0100

    - Fusion master.

--- src/IO.Relational/Data/Meta/MetaSetDb.cs
+++ src/IO.Relational/Data/Meta/MetaSetDb.cs
@@ -1,13 +1,6 @@
 ﻿using System.Collections.Generic;
-<<<<<<<< HEAD:src/IO.Relational/Data/Meta/MetaSetDb.cs
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-========
-using System.ComponentModel;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Text.Json.Serialization;
-using System.Xml.Serialization;
->>>>>>>> 40f26f9d0a8cdbc5927cf14208a3b5386203e2aa:src/IO.Dtos/Data/Meta/Definition/SpecSetDto.cs
 
 namespace BindOpen.Data.Meta
 {
@@ -37,14 +30,7 @@ namespace BindOpen.Data.Meta
         /// <summary>
         /// The elements of this instance.
         /// </summary>
-<<<<<<<< HEAD:src/IO.Relational/Data/Meta/MetaSetDb.cs
         public List<MetaDataDb> Items { get; set; }
-========
-        [NotMapped]
-        [JsonPropertyName("items")]
-        [XmlElement("spec", Type = typeof(SpecDto))]
-        public List<SpecDto> Items { get; set; }
->>>>>>>> 40f26f9d0a8cdbc5927cf14208a3b5386203e2aa:src/IO.Dtos/Data/Meta/Definition/SpecSetDto.cs
 
         #endregion
 
